dbd3a448b6046aa64ad6e6e74ee3e4d628538b37
Jimmy Engström <jimmy@apeoholic.se>
2021-04-24 21:58:52 +0200

    Added chapter 7

---
+++ src/Ch7/MyBlog/MyBlog.Data/MyBlogApiClientSide.cs
@@ -0,0 +1,101 @@
+﻿//<using>
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyBlog.Data.Interfaces;
+using System.Net.Http;
+using MyBlog.Data.Models;
+using System.Net.Http.Json;
+using System;
+//</using>
+namespace MyBlog.Data
+{
+    public class MyBlogApiClientSide : IMyBlogApi
+    {
+        //<Constructor>
+        private readonly IHttpClientFactory factory;
+
+        public MyBlogApiClientSide(IHttpClientFactory factory)
+        {
+            this.factory = factory;
+        }
+        //</Constructor>
+
+        //<BlogpostGet>
+        public async Task<BlogPost> GetBlogPostAsync(int id)
+        {
+            var httpclient = factory.CreateClient("Public");
+            return await httpclient.GetFromJsonAsync<BlogPost>($"MyBlogAPI/BlogPosts/{id}");
+        }
+
+        public async Task<int> GetBlogPostCountAsync()
+        {
+            var httpclient = factory.CreateClient("Public");
+            return await httpclient.GetFromJsonAsync<int>("MyBlogAPI/BlogPostCount");
+        }
+
+        public async Task<List<BlogPost>> GetBlogPostsAsync(int numberofposts, int startindex)
+        {
+            var httpclient = factory.CreateClient("Public");
+            return await httpclient.GetFromJsonAsync<List<BlogPost>>($"MyBlogAPI/BlogPosts?numberofposts={numberofposts}&startindex={startindex}");
+        }
+        //</BlogpostGet>
+        //<BlogpostSaveDelete>
+        public async Task<BlogPost> SaveBlogPostAsync(BlogPost item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteBlogPostAsync(BlogPost item)
+        {
+            throw new NotImplementedException();
+        }
+        //</BlogpostSaveDelete>
+
+        //<Categories>
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            var httpclient = factory.CreateClient("Public");
+            return await httpclient.GetFromJsonAsync<List<Category>>($"MyBlogAPI/Categories");
+        }
+
+        public async Task<Category> GetCategoryAsync(int id)
+        {
+            var httpclient = factory.CreateClient("Public");
+            return await httpclient.GetFromJsonAsync<Category>($"MyBlogAPI/Categories/{id}");
+        }
+
+        public async Task DeleteCategoryAsync(Category item)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Category> SaveCategoryAsync(Category item)
+        {
+            throw new NotImplementedException();
+        }
+        //</Categories>
+
+        //<Tags>
+        public async Task<Tag> GetTagAsync(int id)
+        {
+            var httpclient = factory.CreateClient("Public");
+            return await httpclient.GetFromJsonAsync<Tag>($"MyBlogAPI/Tags/{id}");
+        }
+
+        public async Task<List<Tag>> GetTagsAsync()
+        {
+            var httpclient = factory.CreateClient("Public");
+            return await httpclient.GetFromJsonAsync<List<Tag>>($"MyBlogAPI/Tags");
+        }
+
+        public async Task DeleteTagAsync(Tag item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Tag> SaveTagAsync(Tag item)
+        {
+            throw new NotImplementedException();
+        }
+        //</Tags>
+    }
+}
---
+++ src/Ch7/MyBlog/MyBlogServerSide/Components/BootstrapCssClassProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+using System.Linq;
+using Blazm;
+
+namespace Microsoft.AspNetCore.Components.Forms
+{
+    public class CustomCssClassProvider<T> : ComponentBase where T: FieldCssClassProvider
+    {
+        [CascadingParameter]
+        EditContext? CurrentEditContext { get; set; }
+
+        public FieldCssClassProvider provider { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException($"{nameof(DataAnnotationsValidator)} requires a cascading " +
+                    $"parameter of type {nameof(EditContext)}. For example, you can use {nameof(DataAnnotationsValidator)} " +
+                    $"inside an EditForm.");
+            }
+
+            CurrentEditContext.SetFieldCssClassProvider(provider);
+        }
+    }
+}
